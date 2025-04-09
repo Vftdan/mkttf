7bf9ce48a317ca21d71cdd03d826f80108273f56
Vftdan <vftdan@gmail.com>
2025-04-09 18:11:05 +0200

    Adapt for the creep2 font

--- run.sh
+++ run.sh
@@ -1,12 +1,17 @@
 #! /bin/sh
-if [ "$1" = "-h" -o $# -ne 2 ]; then
+if [ "$1" = "-h" ]; then
 	exec >&2
 	echo 'Usage:'
-	echo " ${0} srcdir fontver [additional mkttf.py options]"
+	echo " ${0} [additional mkttf.py options]"
 	echo 'Set your $TMPDIR to a frequent creation-deletion friendly directory'
 
 	exit 1
 fi
 dir="$(dirname "$0")"
 
-AUTOTRACE="$(realpath "$dir/potrace-blocky.sh")" "$dir"/mkttf.sh "$1" "$2" TerminusPixelated12pt TerminusPixelated12pt
+AUTOTRACE="$(realpath "$dir/potrace-blocky.sh")" "$dir"/mkttf.py -f Creep2 -n Creep2-Regular -N 'Creep2 Regular' -C "; Original copyright (c) 2016 romeovs; Licensed under the MIT License; Modified" -A ' -a -1' -V 2.1 -O "$@" "$dir"/creep2/creep2-11.bdf
+exitcode=$?
+if [ "$exitcode" -gt 0 ]; then \
+	echo "mkttf.py exited with code $exitcode" >&2
+	exit "$exitcode"
+fi
